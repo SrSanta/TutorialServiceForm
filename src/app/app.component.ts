d4fe9b69164073a9e7eac2fc35facec3969a5374
Andrés <andrescondegarcia11@gmail.com>
2025-02-10 19:43:14 +0100

    Empezamos bien ahora si

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FormComponent } from './component/form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tutorialServiceForm';
+  title = 'ServiceForm';
 }
