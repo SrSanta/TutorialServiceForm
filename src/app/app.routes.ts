641c90173e03611ad8eda3fc0f0a31d72b29fff1
Andrés <andrescondegarcia11@gmail.com>
2025-02-15 18:38:31 +0100

    Creo formularios para crear empleados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { EntradaComponent } from './component/entrada/entrada.component';
 import { ListadoComponent } from './component/listado/listado.component';
 import { FormularioComponent } from './component/formulario/formulario.component';
+import { FormularioEmpleadoComponent } from './component/formulario-empleado/formulario-empleado.component';
 
 export const routes: Routes = [
   { path: '', component: EntradaComponent },
   { path: 'listado', component: ListadoComponent },
   { path: 'formulario', component: FormularioComponent },
+  { path: 'formulario-empleado', component: FormularioEmpleadoComponent },
 ];
---
+++ src/app/component/formulario-empleado/formulario-empleado.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { EmpleadosService } from '../../services/empleado.service';
+import { Empleado } from '../../models/persona.model';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import e from 'express';
+
+@Component({
+  selector: 'app-formulario-empleado',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './formulario-empleado.component.html',
+  styleUrl: './formulario-empleado.component.css'
+})
+export class FormularioEmpleadoComponent {
+  empleados: Empleado[] = [];
+  formEmp: FormGroup;
+  nombre: string = '';
+
+  constructor(private empleadosService: EmpleadosService, private fb: FormBuilder) {
+    this.formEmp = this.fb.group({
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      departamento: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+    this.empleadosService.getEmpleados().subscribe((empleados) => {
+      this.empleados = empleados;
+    });
+  }
+
+  addEmpleado(formEmp: FormGroup) {
+    if (formEmp.valid) {
+      this.empleadosService.addEmpleado(formEmp.value).subscribe(() => {
+        this.empleados.push(formEmp.value);
+      });
+    }
+  }
+
+  submit() {
+    const nuevoEmpleado: Empleado = {
+      id: this.empleados.length + 1,
+      nombre: this.formEmp.value.nombre,
+      apellido: this.formEmp.value.apellido,
+      email: this.formEmp.value.email,
+      departamento: this.formEmp.value.departamento
+    };
+
+    this.empleadosService.addEmpleado(nuevoEmpleado).subscribe(() => {
+      this.empleados.push(nuevoEmpleado);
+    });
+
+    this.formEmp.reset();
+  }
+}
